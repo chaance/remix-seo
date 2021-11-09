898e2e84b807b809abbda044df5f136a68d03aab
Chance Strickland <hi@chance.dev>
2021-11-09 00:16:28 -0800

    simplify robots stuff

--- src/index.ts
+++ src/index.ts
@@ -170,28 +170,31 @@ export function getSeo(defaultConfig?: SeoConfig): SeoFunction {
 			unavailableAfter,
 		} = robots;
 
-		let robotsParam = joinTruthy(",", [
-			noSnippet && "nosnippet",
-			maxSnippet && `max-snippet:${maxSnippet}`,
-			maxImagePreview && `max-image-preview:${maxImagePreview}`,
+		let robotsParams = [
 			noArchive && "noarchive",
-			unavailableAfter && `unavailable_after:${unavailableAfter}`,
 			noImageIndex && "noimageindex",
-			maxVideoPreview && `max-video-preview:${maxVideoPreview}`,
+			noSnippet && "nosnippet",
 			noTranslate && `notranslate`,
-		]);
+			maxImagePreview && `max-image-preview:${maxImagePreview}`,
+			maxSnippet && `max-snippet:${maxSnippet}`,
+			maxVideoPreview && `max-video-preview:${maxVideoPreview}`,
+			unavailableAfter && `unavailable_after:${unavailableAfter}`,
+		];
 
-		meta.robots = joinTruthy(",", [
-			noIndex ? "noindex" : "index",
-			noFollow ? "nofollow" : "follow",
-			robotsParam,
-		]);
+		let robotsParam =
+			(noIndex ? "noindex" : "index") +
+			"," +
+			(noFollow ? "nofollow" : "follow");
+
+		for (let param of robotsParams) {
+			if (param) {
+				robotsParam += `,${param}`;
+			}
+		}
+
+		meta.robots = robotsParam;
 		if (!omitGoogleBotMeta) {
-			meta.googlebot = joinTruthy(",", [
-				noIndex ? "noindex" : "index",
-				noFollow ? "nofollow" : "follow",
-				robotsParam,
-			]);
+			meta.googlebot = meta.robots;
 		}
 
 		// <link rel="alternate">
@@ -565,10 +568,6 @@ function warn(message: string): void {
 	} catch (e) {}
 }
 
-function joinTruthy(separator: string, array: any[]) {
-	return [...array].filter(Boolean).join(separator);
-}
-
 function warnIfInvalidUrl(str: string, warning: string) {
 	try {
 		new URL(str);
@@ -669,16 +668,133 @@ interface LanguageAlternate {
 
 type ImagePrevSize = "none" | "standard" | "large";
 
+/**
+ * @see https://developers.google.com/search/docs/advanced/robots/robots_meta_tag
+ */
 interface RobotsOptions {
+	/**
+	 * Set the maximum size of an image preview for this page in a search results.
+	 *
+	 * If false, Google may show an image preview of the default size.
+	 *
+	 * Accepted values are:
+	 *
+	 * - **none:** No image preview is to be shown.
+	 * - **standard:** A default image preview may be shown.
+	 * - **large:** A larger image preview, up to the width of the viewport, may
+	 *   be shown.
+	 *
+	 * This applies to all forms of search results (such as Google web search,
+	 * Google Images, Discover, Assistant). However, this limit does not apply in
+	 * cases where a publisher has separately granted permission for use of
+	 * content. For instance, if the publisher supplies content in the form of
+	 * in-page structured data (such as AMP and canonical versions of an article)
+	 * or has a license agreement with Google, this setting will not interrupt
+	 * those more specific permitted uses.
+	 *
+	 * If you don't want Google to use larger thumbnail images when their AMP
+	 * pages and canonical version of an article are shown in Search or Discover,
+	 * provide a value of `"standard"` or `"none"`.
+	 */
 	maxImagePreview?: ImagePrevSize;
+	/**
+	 * The maximum of number characters to use as a textual snippet for a search
+	 * result. (Note that a URL may appear as multiple search results within a
+	 * search results page.)
+	 *
+	 * This does **not** affect image or video previews. This applies to all forms
+	 * of search results (such as Google web search, Google Images, Discover,
+	 * Assistant). However, this limit does not apply in cases where a publisher
+	 * has separately granted permission for use of content. For instance, if the
+	 * publisher supplies content in the form of in-page structured data or has a
+	 * license agreement with Google, this setting does not interrupt those more
+	 * specific permitted uses. This directive is ignored if no parseable value is
+	 * specified.
+	 *
+	 * Special values:
+	 * - 0: No snippet is to be shown. Equivalent to nosnippet.
+	 * - 1: Google will choose the snippet length that it believes is most
+	 *   effective to help users discover your content and direct users to your
+	 *   site.
+	 *
+	 * To specify that there's no limit on the number of characters that can be
+	 * shown in the snippet, `maxSnippet` should be set to `-1`.
+	 */
 	maxSnippet?: number;
+	/**
+	 * The maximum number of seconds for videos on this page to show in search
+	 * results.
+	 *
+	 * If false, Google may show a video snippet in search results and will decide
+	 * how long the preview may be.
+	 *
+	 * Special values:
+	 *
+	 * - 0: At most, a static image may be used, in accordance to the
+	 *   `maxImagePreview` setting.
+	 * - 1: There is no limit.
+	 *
+	 * This applies to all forms of search results (at Google: web search, Google
+	 * Images, Google Videos, Discover, Assistant).
+	 */
 	maxVideoPreview?: number;
+	/**
+	 * Do not show a cached link in search results.
+	 *
+	 * If false, Google may generate a cached page and users may access it through
+	 * the search results.
+	 */
 	noArchive?: boolean;
+	/**
+	 * Do not follow the links on this page.
+	 *
+	 * If false, Google may use the links on the page to discover those linked
+	 * pages.
+	 *
+	 * @see https://developers.google.com/search/docs/advanced/guidelines/qualify-outbound-links
+	 */
 	noFollow?: boolean;
+	/**
+	 * Do not index images on this page.
+	 *
+	 * If false, images on the page may be indexed and shown in search results.
+	 */
 	noImageIndex?: boolean;
+	/**
+	 * Do not show this page, media, or resource in search results.
+	 *
+	 * If false, the page, media, or resource may be indexed and shown in search
+	 * results.
+	 */
 	noIndex?: boolean;
+	/**
+	 * Do not show a text snippet or video preview in the search results for this
+	 * page. A static image thumbnail (if available) may still be visible, when it
+	 * results in a better user experience. This applies to all forms of search
+	 * results (at Google: web search, Google Images, Discover).
+	 *
+	 * If false, Google may generate a text snippet and video preview based on
+	 * information found on the page.
+	 */
 	noSnippet?: boolean;
+	/**
+	 * Do not offer translation of this page in search results.
+	 *
+	 * If false, Google may show a link next to the result to help users view
+	 * translated content on your page.
+	 */
 	noTranslate?: boolean;
+	/**
+	 * Do not show this page in search results after the specified date/time.
+	 *
+	 * The date/time must be specified in a widely adopted format including, but
+	 * not limited to [RFC 822](http://www.ietf.org/rfc/rfc0822.txt), [RFC
+	 * 850](http://www.ietf.org/rfc/rfc0850.txt), and [ISO
+	 * 8601](https://www.iso.org/iso-8601-date-and-time-format.html). The
+	 * directive is ignored if no valid date/time is specified.
+	 *
+	 * By default there is no expiration date for content.
+	 */
 	unavailableAfter?: string;
 }
 
